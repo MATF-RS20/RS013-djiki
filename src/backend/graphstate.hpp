cd12eb64c734f606b4e0d50590d5ebf7309caa76
Konstantin <konstantin.klima@live.com>
2019-12-24 00:16:52 +0100

    Added graph state class for following algorithm execution

--- src/backend/graphstate.hpp
+++ src/backend/graphstate.hpp
@@ -1,4 +1,23 @@
 #ifndef GRAPHSTATE_H
 #define GRAPHSTATE_H
 
+#include "../graph/node.hpp"
+#include "../graph/edge.hpp"
+
+
+struct GraphState {
+    GraphState() {}
+    GraphState(Node* currentNode, Edge* currentEdge, unsigned currentLine)
+        : currentNode(currentNode)
+        , currentEdge(currentEdge)
+        , currentLine(currentLine)
+    {}
+
+    Node* currentNode;
+    Edge* currentEdge;
+    unsigned currentLine;
+};
+
+
+
 #endif // GRAPHSTATE_H
